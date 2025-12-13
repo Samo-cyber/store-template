e2416506f3c427d6c65746a23f508d0683e79ce1
Samo-cyber <123793553+Samo-cyber@users.noreply.github.com>
2025-12-13 03:46:01 -0800

    صص

--- src/components/templates/index.tsx
+++ src/components/templates/index.tsx
@@ -1,7 +1,7 @@
 import ModernTemplate from "./ModernTemplate";
 import ClassicTemplate from "./ClassicTemplate";
 
-export const templates = {
+export const templates: Record<string, (props: { storeId: string }) => Promise<JSX.Element> | JSX.Element> = {
     modern: ModernTemplate,
     classic: ClassicTemplate,
 };
